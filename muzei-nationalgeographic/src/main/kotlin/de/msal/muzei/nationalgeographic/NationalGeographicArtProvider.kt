9b7cc6eb78f7f726949641f01362973bd11eea1e
Max <msal.coding@gmail.com>
2019-12-06 21:04:46 +0100

    Use more Kotlin...

--- muzei-nationalgeographic/src/main/kotlin/de/msal/muzei/nationalgeographic/NationalGeographicArtProvider.kt
+++ muzei-nationalgeographic/src/main/kotlin/de/msal/muzei/nationalgeographic/NationalGeographicArtProvider.kt
@@ -23,25 +23,35 @@ class NationalGeographicArtProvider : MuzeiArtProvider() {
          UserCommand(USER_COMMAND_ID_PHOTO_DESCRIPTION, context?.getString(R.string.photo_desc_open)),
          UserCommand(USER_COMMAND_ID_SHARE, context?.getString(R.string.share_artwork_title)))
 
+   /*
+    * Not working properly in Android 10. See:
+    * - https://github.com/romannurik/muzei/issues/644
+    * - https://developer.android.com/guide/components/activities/background-starts
+    */
    override fun onCommand(artwork: Artwork, id: Int) {
+      val context = context ?: return
       when (id) {
          USER_COMMAND_ID_PHOTO_DESCRIPTION -> {
-            val intent = Intent(context, PhotoDescriptionActivity::class.java)
-            intent.putExtra(PhotoDescriptionActivity.EXTRA_TITLE, artwork.title)
-            intent.putExtra(PhotoDescriptionActivity.EXTRA_DESC, artwork.token)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            context?.startActivity(intent)
+            Intent(context, PhotoDescriptionActivity::class.java).apply {
+               putExtra(PhotoDescriptionActivity.EXTRA_TITLE, artwork. title)
+               putExtra(PhotoDescriptionActivity.EXTRA_DESC, artwork.token)
+               flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            }.takeIf { it.resolveActivity(context.packageManager) != null }?.run {
+               context.startActivity(this)
+            }
          }
          USER_COMMAND_ID_SHARE -> {
-            var shareIntent = Intent(Intent.ACTION_SEND)
-            shareIntent.type = "text/plain"
-            shareIntent.putExtra(Intent.EXTRA_TEXT, context?.getString(R.string.share_artwork_message,
-                  artwork.title,
-                  artwork.byline,
-                  artwork.webUri))
-            shareIntent = Intent.createChooser(shareIntent, context?.getString(R.string.share_artwork_title))
-            shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            context?.startActivity(shareIntent)
+            Intent.createChooser(Intent(Intent.ACTION_SEND).apply {
+               type = "text/plain"
+               putExtra(Intent.EXTRA_TEXT, context.getString(R.string.share_artwork_message,
+                     artwork.title,
+                     artwork.byline,
+                     artwork.webUri))
+            }, context.getString(R.string.share_artwork_title))?.apply {
+               addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }?.takeIf { it.resolveActivity(context.packageManager) != null }?.run {
+               context.startActivity(this)
+            }
          }
       }
    }
